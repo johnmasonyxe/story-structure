c24b67ccda7741aec20a5e0ddb230ad01b6bafc0
johnmason <john.mason.yxe@gmail.com>
2021-03-05 14:10:05 -0600

    add turner novel framework

---
+++ turner-book-outline.ts
@@ -0,0 +1,145 @@
+// https://www.turnerstories.com/blog/2019/9/12/a-great-framework-for-plotting-a-fantasy-novel
+export const turner_fantasy_novel_framework = () => {
+    step_1_introduce_status_quo: () => {
+        introuduce_world: () => {}
+        introuduce_setting: () => {}
+        introduce_theme: () => {}
+        show_character_flaws: () => {}
+    }
+
+    step_2_inciting_incident: () => {
+        attack: () => {}
+        important_discovery: () => {}
+        leaves_status_quo: () => {}
+        character_has_new_goals: () => {}
+        tease_future_add_excitement: () => {}
+    }
+
+    step_3_act_1_setting_and_predicament: () => {
+        show_reader_new_world: () => {
+            what_is_happening: () => {}
+            how_is_main_character_invovled: () => {}
+            who_does_main_character_meet: () => {}
+            introduce_antagonists: () => {}
+            what_new_goals_does_character_have: () => {}
+        }
+        goal_is_far_beyond_reach_of_character: () => {
+            much_improvement_needed_to_achieve_goals: () => {
+                overcome_flaws: () => {}
+                skills_to_master: () => {}
+                items_to_get: () => {}
+            }
+        }
+        main_character_is_put_in_central_position_of_the_story: () => {
+            only_they_can_do_something: () => {}
+            something_is_unique_about_them: () => {
+                magic_blood: () => {}
+                special_skill: () => {}
+                special_birth: () => {}
+                chosen_by_ruling_cast: () => {}
+                unlucky_or_lucky: () => {}
+            }
+        }
+    }
+
+    step_4_act_1_lock_in_reader: () => {
+        the_goal_propells_main_character: () => {}
+        main_character_does_not_want_to_do_goal: () => {}
+        starts_journey_of_self_discovery_and_growth: () => {}
+        journey_that_reader_is_excited_to_be_part_of: () => {}
+        main_character_accepts_their_role_in_the_story: () => {
+            scared_as_hell: () => {}
+            unsure_of_themself: () => {}
+            about_to_start_trials: () => {
+                is_not_skilled_enough_to_overcome_them: () => {}
+            }
+        }
+    }
+
+    step_5_act_2_throw_in_some_obstacles: () => {
+        main_character_is_on_mission: () => {}
+        get_to_know_companions: () => {}
+        tasks_and_learning_from_mentor: () => {}
+        antagonist_throws_obstacles_at_main_character: () => {}
+        obstacles_appear_everywhere: () => {
+            main_character_is_not_strong_enough_to_overcome_them: () => {}
+            antagonist_is_too_strong: () => {}
+        }
+    }
+
+    step_6_act_2_rising_action: () => {
+        main_character_has_some_mini_quests_under_their_belt: () => {
+            starting_to_understand_world: () => {}
+            growth_is_happening: () => {}
+            wins_start_to_happen: () => {}
+        }
+        main_character_starts_to_believe_they_can_accomplish_ultimate_goal: () => {}
+    }
+
+    step_7_act_2_subplots_and_more_action: () => {
+        smooth_sailing: () => {}
+        side_quests_surface: () => {}
+        other_story_promises_appear: () => {}
+        romantic_climax_happens: () => {}
+        side_quest_that_will_be_main_quest_in_future_book_surfaces: () => {}
+        antagonist_is_growing_as_well: () => {
+            tries_to_block_main_characters_path: () => {}
+        }
+    }
+
+    step_8_act_2_the_turning_point: () => {
+        main_charcter_is_ready_to_take_on_main_obstacle: () => {}
+        minor_setback: () => {
+            loses_to_an_antagonist: () => {}
+            doubt_creeps_in: () => {}
+        }
+        main_character_must_learn_they_are_only_one_that_can_succeed: () => {
+            friends_try_to_help: () => {}
+            no_one_can_do_it_for_them: () => {}
+        }
+        set_up_grand_finale: () => {
+            help_reader_understand_plan: () => {}
+            make_it_clear_what_main_character_has_to_do: () => {}
+            create_doubt_and_anxiety_about_climax: () => {}
+        }
+    }
+
+    step_9_act_3_tensions_twists_turns: () => {
+        reader_is_at_edge_of_seat: () => {
+            cant_put_book_down_must_know: () => {}
+        }
+        story_swings_into_full_action: () => {}
+        main_character_takes_action_follows_plan: () => {
+            main_character_doesnt_feel_ready: () => {}
+            faces_own_insecurities_and_flaws_and_bravely_goes_anyway: () => {}
+            ready_to_make_sacrifice: () => {}
+            reflects_on_everything_they_have_gone_through: () => {}
+        }
+        main_character_battles_main_antagonist: () => {
+            main_character_is_close_to_changing_to_evolving: () => {}
+            they_fight_a_bitter_battle: () => {}
+            main_character_changes_and_overcomes_their_own_demons_and_the_antagonist: () => {}
+        }
+    }
+
+    step_10_act_3_the_climax: () => {
+        the_battle_is_over_main_character_has_won: () => {}
+        now_there_is_a_cost_to_pay_for_winning: () => {
+            sacrifice_of_freedom: () => {}
+            physical_sacrifice: () => {
+                lost_limb: () => {}
+                lost_sence: () => {}
+                lost_wealth: () => {}
+            }
+            sacrifice_of_who_main_character_was: () => {
+                loss_of_innocence: () => {}
+                burden_of_new_responsibility: () => {}
+            }
+        }
+        main_character_accepts_their_place_in_the_new_world: () => {}
+        make_it_clear_to_reader_main_character_has_reached_external_goal: () => {}
+        subplots_are_tied_up: () => {}
+        
+    }
+
+}
